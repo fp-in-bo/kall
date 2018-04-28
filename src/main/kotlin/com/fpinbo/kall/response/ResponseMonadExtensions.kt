8c573ab1e9765b02da7d8f64bc263aff03663774
Alessandro Zanin <ale.zanin90@gmail.com>
2018-04-28 12:14:20 +0200

    Move fold,flatMap e map in ResponseExtension

--- src/main/kotlin/com/fpinbo/kall/response/ResponseMonadExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.fpinbo.kall.response
-
-fun <A, B> Response<A>.flatMap(f: (A) -> Response<B>): Response<B> =
-        fold({ Response.Error(it.errorBody, code, headers, message) }, { f(it.body) })
-
